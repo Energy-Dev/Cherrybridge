f9b4406d48a1777ebda2f950f80e7e3d5d9d33f0
Energy-Dev <niklas.kastel@googlemail.com>
2014-08-10 15:59:59 +0200

    index.php

--- index.php
+++ index.php
@@ -1,6 +1,13 @@
 <html>
-  <head>
-  </head>
+	<head>
+		<meta name="description" content="Cherrybridge - Content-Management und Blogsystem von Energy-Development">
+		<meta name="keywords" content="Cherry, cherry, brigde, Bridge, CMS, Content, Management, System, Blog, Blogsystem, Blog, Energy, Dev, Development, Energy-Dev, Energy-Development">
+		<meta name="robots" content="index,follow">
+		<link href="styles/style.css" type="text/css" rel="stylesheet">
+		<link rel="shortcut icon" href="images/favicon.png" type="image/x-icon">
+		<meta charset="utf-8">		
+		<title>Cherrybridge</title>
+	</head>
   <body>
     <?php
     ?>
